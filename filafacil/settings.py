f37dbce0cbb041792bf09c65d3d2ce6775e1ac94
adbrum <adbrum@outlook.com>
2016-03-04 17:04:56 +0000

    Add layout basic, tests

--- filafacil/settings.py
+++ filafacil/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'filafacil.core',
+    # 'filafacil.accesspoint',
+    'filafacil.screening',
 ]
 
 MIDDLEWARE_CLASSES = [
